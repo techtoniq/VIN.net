e52905849815f845fbdc4a601e1ae91529e53df3
Matt Daniels <matt@techtoniq.com>
2024-01-12 14:15:38 +0000

    Basic solution structure.

---
+++ Src/VehicleIdentificationNumberLib/Vin.cs
@@ -0,0 +1,26 @@
+﻿namespace VehicleIdentificationNumberLib
+{
+    public class Vin
+    {
+        #region Private Fields
+
+        private readonly string _value;
+
+        #endregion Private Fields
+
+        #region Ctor
+
+        public Vin(string value)
+        {
+            if(string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+
+            if(value.Length != 17)
+                throw new ArgumentException("Invalid length (must be 17 characters)", nameof(value));
+
+            _value = value;
+        }
+
+        #endregion Ctor
+    }
+}
---
+++ Test/VehicleIdentificationNumberLib.UnitTest/VinTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace VehicleIdentificationNumberLib.UnitTest
+{
+    public class VinTests
+    {
+        public class Constructor
+        {
+            [Test]
+            public void CannotBeEmpty()
+            {
+                Action act = () => new Vin(string.Empty);
+
+                act.Should().Throw<ArgumentNullException>()
+                    .WithMessage("Value cannot be null. (Parameter 'value')");
+            }
+        }
+    }
+}
