1410103d0e24017642941c9604d955ee5caa3773
Matt Daniels <matt@techtoniq.com>
2024-01-13 06:26:52 +0000

    Completed simple Vin object with basic validation.

--- Src/VehicleIdentificationNumberLib/Vin.cs
+++ Src/VehicleIdentificationNumberLib/Vin.cs
@@ -18,9 +18,52 @@
             if(value.Length != 17)
                 throw new ArgumentException("Invalid length (must be 17 characters)", nameof(value));
 
+            for(int i = 0; i < value.Length; i++)
+            {
+                if ((value[i] < 'A' || value[i] > 'Z') && !Char.IsDigit(value[i]))
+                    throw new ArgumentException("All characters must be upper case alphanumeric only", nameof(value));
+
+                if (value[i] == 'I' || value[i] == 'O' || value[i] == 'Q')
+                    throw new ArgumentException($"Invalid character at position {i}", nameof(value));
+            }
+
             _value = value;
         }
 
         #endregion Ctor
+
+        #region Public Operators
+
+        public static explicit operator Vin(string value)
+        {
+            Vin vin = new Vin(value);
+            return vin;
+        }
+
+        #endregion Public Operators
+
+        #region Public Object Overrides
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Vin;
+
+            if (other == null)
+                return false;
+
+            return _value.Equals(other._value);
+        }
+
+        #endregion Public Object Overrides
     }
 }
--- Test/VehicleIdentificationNumberLib.UnitTest/VinTests.cs
+++ Test/VehicleIdentificationNumberLib.UnitTest/VinTests.cs
@@ -7,12 +7,125 @@ namespace VehicleIdentificationNumberLib.UnitTest
         public class Constructor
         {
             [Test]
-            public void CannotBeEmpty()
+            public void CannotBeNull()
             {
-                Action act = () => new Vin(string.Empty);
+                Action act = () => { Vin v = new Vin(null!); };
 
                 act.Should().Throw<ArgumentNullException>()
                     .WithMessage("Value cannot be null. (Parameter 'value')");
+            }
+
+            [Test]
+            public void CannotBeEmpty()
+            {
+                Action act = () => { Vin v = new Vin("   "); };
+
+                act.Should().Throw<ArgumentNullException>()
+                    .WithMessage("Value cannot be null. (Parameter 'value')");
+            }
+
+            [Test]
+            public void CannotBeLessThan17CharactersLong()
+            {
+                Action act = () => { Vin v = new Vin("1234567890123456"); };
+
+                act.Should().Throw<ArgumentException>()
+                    .WithMessage("Invalid length (must be 17 characters) (Parameter 'value')");
+            }
+
+            [Test]
+            public void CannotBeMoreThan17CharactersLong()
+            {
+                Action act = () => { Vin v = new Vin("123456789012345678"); };
+
+                act.Should().Throw<ArgumentException>()
+                    .WithMessage("Invalid length (must be 17 characters) (Parameter 'value')");
+            }
+
+            [TestCase("1HGBH41JXMN10x106")]
+            [TestCase("1HGBH41JXMN10%106")]
+            public void MustBeAllUpperCaseAlphanumeric(string value)
+            {
+                Action act = () => { Vin v = new Vin(value); };
+
+                act.Should().Throw<ArgumentException>()
+                    .WithMessage("All characters must be upper case alphanumeric only (Parameter 'value')");
+            }
+
+            [TestCase('I')]
+            [TestCase('O')]
+            [TestCase('Q')]
+            public void MustNotContainIllegalCharacter(Char c)
+            {
+                Action act = () => { Vin v = new Vin($"1HGBH41JXMN10{c}106"); };
+
+                act.Should().Throw<ArgumentException>()
+                    .WithMessage("Invalid character at position 13 (Parameter 'value')");
+            }
+        }
+
+        public class ExplicitConversionOperator
+        {
+            [Test]
+            public void HasExplicitConversionFromString()
+            {
+                string s = "1HGBH41JXMN109106";
+
+                Action act = () => { Vin v = (Vin)s; };
+
+                act.Should().NotThrow();                
+            }
+        }
+
+        public class ToString
+        {
+            [Test]
+            public void ToStringEqualsSourceValue()
+            {
+                string s = "1HGBH41JXMN109106";
+
+                Vin v = new Vin(s);
+
+                v.ToString().Should().Be(s);
+            }
+        }
+
+        public class GetHashCode
+        {
+            [Test]
+            public void HashCodeEqualsSourceValue()
+            {
+                string s = "1HGBH41JXMN109106";
+
+                Vin v = new Vin(s);
+
+                v.GetHashCode().Should().Be(s.GetHashCode());
+            }
+        }
+
+        public class Equals
+        {
+            [Test]
+            public void SameVinValueIsEqual()
+            {
+                string s = "1HGBH41JXMN109106";
+
+                Vin first = new Vin(s);
+                Vin second = new Vin(s);
+
+                first.Should().Be(second);
+            }
+
+            [Test]
+            public void DifferentVinValueIsNotEqual()
+            {
+                string s = "1HGBH41JXMN109106";
+                string t = "1HGBH41JXMN109107";
+
+                Vin first = new Vin(s);
+                Vin second = new Vin(t);
+
+                first.Should().NotBe(second);
             }
         }
     }
